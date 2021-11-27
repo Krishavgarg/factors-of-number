cf529231d69f56b4529b49566899cefc3cf0a53a
AVIRAL108 <vrlgarg@gmail.com>
2021-11-27 14:44:10 +0530

    seventh commit

---
+++ factor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class factor
+{static void main()
+    {int i=1;Scanner sc=new Scanner(System.in);int a=sc.nextInt();int b=sc.nextInt();
+    {while(i<=a)
+        if(a%i==0)
+        {System.out.println(i);
+            i++;
+    }
+else 
+i++;}System.out.print(b);}
+}
